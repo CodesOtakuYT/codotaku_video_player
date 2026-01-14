7ce21e61421ae01106cfcb009725df8d1085ae03
CodesOtakuYT <taouaouilyas@gmail.com>
2026-01-14 05:55:48 +0100

    second part

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include <libavformat/avformat.h>
 #include <libavcodec/avcodec.h>
 
-int main(void) {
+int main(int argc, char **argv) {
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *window;
     SDL_Renderer *renderer;
@@ -11,38 +11,64 @@ int main(void) {
 
     AVFormatContext *format_context = NULL;
     avformat_open_input(&format_context,
-                        "/home/codotaku/Downloads/YTDown.com_YouTube_Big-Buck-Bunny-60fps-4K-Official-Blender_Media_aqz-KE-bpKQ_001_1080p.mp4",
+                        argv[1],
                         NULL, NULL);
     const AVCodec *codec = NULL;
     const int video_stream_index = av_find_best_stream(format_context, AVMEDIA_TYPE_VIDEO, -1, -1, &codec, 0);
+    const AVStream *video_stream = format_context->streams[video_stream_index];
     AVCodecContext *decoder = avcodec_alloc_context3(codec);
     decoder->thread_count = 0;
-    avcodec_parameters_to_context(decoder, format_context->streams[video_stream_index]->codecpar);
+    avcodec_parameters_to_context(decoder, video_stream->codecpar);
     avcodec_open2(decoder, codec, NULL);
 
-    AVPacket packet;
+    AVPacket *packet = av_packet_alloc();
     AVFrame *frame = av_frame_alloc();
 
     SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_YV12, SDL_TEXTUREACCESS_STREAMING,
                                              decoder->width,
                                              decoder->height);
 
+    Uint64 start_ns = 0;
+
     while (true) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) if (event.type == SDL_EVENT_QUIT) return EXIT_SUCCESS;
 
-        if (av_read_frame(format_context, &packet) >= 0) {
-            if (packet.stream_index == video_stream_index) {
-                avcodec_send_packet(decoder, &packet);
-                if (avcodec_receive_frame(decoder, frame) == 0) {
+        if (av_read_frame(format_context, packet) >= 0) {
+            if (packet->stream_index == video_stream_index) {
+                avcodec_send_packet(decoder, packet);
+                while (avcodec_receive_frame(decoder, frame) == 0) {
+                    const double frame_time_s = (double) frame->pts * av_q2d(video_stream->time_base);
+                    if (start_ns == 0) start_ns = SDL_GetTicksNS();
+                    const Uint64 elapsed_time_ns = SDL_GetTicksNS() - start_ns;
+                    const double elapsed_time_s = (double) elapsed_time_ns / SDL_NS_PER_SECOND;
+                    const double delay_s = frame_time_s - elapsed_time_s;
+                    if (delay_s > 0)
+                        SDL_Delay((Uint32) (delay_s * SDL_MS_PER_SECOND));
+                    else if (delay_s < 0.5)
+                        continue;
+
                     SDL_UpdateYUVTexture(texture, NULL, frame->data[0], frame->linesize[0], frame->data[1],
                                          frame->linesize[1], frame->data[2], frame->linesize[2]);
+
+                    int w, h;
+                    SDL_GetCurrentRenderOutputSize(renderer, &w, &h);
+                    const float frame_width = (float) decoder->width;
+                    const float frame_height = (float) decoder->height;
+                    const float scale_w = (float) w / frame_width;
+                    const float scale_h = (float) h / frame_height;
+                    const float scale = SDL_min(scale_w, scale_h);
+                    SDL_FRect dstrect;
+                    dstrect.w = frame_width * scale;
+                    dstrect.h = frame_height * scale;
+                    dstrect.x = ((float) w - dstrect.w) / 2;
+                    dstrect.y = ((float) h - dstrect.h) / 2;
+
+                    SDL_RenderTexture(renderer, texture, NULL, &dstrect);
+                    SDL_RenderPresent(renderer);
                 }
             }
-            av_packet_unref(&packet);
+            av_packet_unref(packet);
         }
-
-        SDL_RenderTexture(renderer, texture, NULL, NULL);
-        SDL_RenderPresent(renderer);
     }
 }
